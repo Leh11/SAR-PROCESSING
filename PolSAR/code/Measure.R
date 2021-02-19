4242111a744a0f5f55c570be5df627dccc3facf4
leh11 <ui@laccan.ufal.br>
2021-02-19 19:51:08 -0300

    Histogram sample add

--- PolSAR/code/Measure.R
+++ PolSAR/code/Measure.R
@@ -1,5 +1,6 @@
 require("colordistance")
 require("colorspace")
+require(plot3D)
 
 # read the ideal "pauli"
 ideal <- loadImage("pauli.png", 
@@ -54,3 +55,103 @@ h.ideal_lighthouse.L <- hist(ideal_lighthouse$filtered.lab.2d$L,
 h.ideal_mount.L <- hist(ideal_mount$filtered.lab.2d$L, 
                            main = "Histogram of L components of mount", 
                            xlab = "L component")
+
+## ===============================================================================
+## ==                             3D Histogram                                  ==         
+## ===============================================================================
+
+ideal.Pauli <- as.matrix(ideal$filtered.lab.2d)
+ideal.Lena <- as.matrix(ideal_lena$filtered.lab.2d)
+ideal.mandrill <- as.matrix(ideal_mandrill$filtered.lab.2d)
+ideal.lighthouse <- as.matrix(ideal_lighthouse$filtered.lab.2d)
+ideal.mount <- as.matrix(ideal_mount$filtered.lab.2d)
+
+par(mfrow = c(2, 5))
+hist3D(z = ideal.Pauli[1:20, ], scale = F, 
+       expand = 0.01,
+       theta = 40, phi = 50,
+       border = "black", shade = 0.2,
+       space = 0.2, ticktype = "detailed",
+       main = "Histogram of pauli with theta = 40 and phi = 40 using hist3D()")
+
+hist3D(z = ideal.Lena[1:20, ], scale = F, 
+       expand = 0.01,
+       theta = 40, phi = 50,
+       border = "black", shade = 0.2,
+       space = 0.2, ticktype = "detailed",
+       main = "Histogram of Lena with theta = 40 and phi = 40 using hist3D()")
+
+hist3D(z = ideal.mandrill[1:20, ], scale = F, 
+       expand = 0.01,
+       theta = 40, phi = 50,
+       border = "black", shade = 0.2,
+       space = 0.2, ticktype = "detailed",
+       main = "Histogram of mandrill with theta = 40 and phi = 40 using hist3D()")
+
+hist3D(z = ideal.lighthouse[1:20, ], scale = F, 
+       expand = 0.01,
+       theta = 40, phi = 50,
+       border = "black", shade = 0.2,
+       space = 0.2, ticktype = "detailed",
+       main = "Histogram of lighthouse with theta = 40 and phi = 40 using hist3D()")
+
+
+hist3D(z = ideal.mount[1:20, ], scale = F, 
+       expand = 0.01,
+       theta = 40, phi = 50,
+       border = "black", shade = 0.2,
+       space = 0.2, ticktype = "detailed",
+       main = "Histogram of mount with theta = 40 and phi = 40 using hist3D()")
+
+#======
+getLabHist(ideal, 
+           sample.size = 'null',
+           bins = c(4, 4, 4), 
+           color.space = "CIELab",
+           ref.white = "D65",
+           plotting = T,
+           a.bounds = c(-127, 127),
+           b.bounds = c(-127, 127),
+           title = "Histogram of lena using getLabHist()")
+
+getLabHist(ideal_lena, 
+           sample.size = 'null',
+           bins = c(4, 4, 4), 
+           color.space = "CIELab",
+           ref.white = "D65",
+           plotting = T,
+           a.bounds = c(-127, 127),
+           b.bounds = c(-127, 127),
+           title = "Histogram of lena using getLabHist()")
+
+getLabHist(ideal_mandrill, 
+           sample.size = 'null',
+           bins = c(4, 4, 4), 
+           color.space = "CIELab",
+           ref.white = "D65",
+           plotting = T,
+           a.bounds = c(-127, 127),
+           b.bounds = c(-127, 127),
+           title = "Histogram of mandrill using getLabHist()")
+
+getLabHist(ideal_lighthouse, 
+           sample.size = 'null',
+           bins = c(4, 4, 4), 
+           color.space = "CIELab",
+           ref.white = "D65",
+           plotting = T,
+           a.bounds = c(-127, 127),
+           b.bounds = c(-127, 127),
+           title = "Histogram of lighthouse using getLabHist()")
+
+
+getLabHist(ideal_mount, 
+           sample.size = 'null',
+           bins = c(4, 4, 4), 
+           color.space = "CIELab",
+           ref.white = "D65",
+           plotting = T,
+           a.bounds = c(-127, 127),
+           b.bounds = c(-127, 127),
+           title = "Histogram of mount using getLabHist()")
+
