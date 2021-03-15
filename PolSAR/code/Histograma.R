d22f2041552b5fff5a1abd538c15f6030499fd82
leh11 <ui@laccan.ufal.br>
2021-03-15 16:40:44 -0300

    Överlying histogram

---
+++ PolSAR/code/Histograma.R
@@ -0,0 +1,35 @@
+require(colordistance)
+require(colorspace)
+require(GGally)
+require(ggthemes)
+require(cowplot)
+require(ggplot2)
+require(distrEx)
+
+## ==================================================================
+## ==                         Histogramas                          ==
+## ==================================================================
+
+# read the ideal "pauli"
+ideal <- loadImage("pauli.png", 
+                   CIELab = TRUE, sample.size = NULL, ref.white = "D65",
+                   lower = NULL, upper = NULL)
+
+# get data from the Lab and RGB space
+frame_LAB <- data.frame(ideal$filtered.lab.2d)
+frame_RGB <- data.frame(ideal$filtered.rgb.2d)
+
+names(frame_RGB) <- c("R", "G", "B")
+
+all_points <- cbind(frame_LAB, frame_RGB)
+
+# overlying the histogram using ggplot
+ggplot(all_points) +
+      geom_histogram(aes(x = a, fill = "a", alpha = .3),
+                     col = "white", bins = 70) +
+      geom_histogram(aes(x = b, fill = "b", alpha = .3),
+                     col = "white", bins = 70) +
+      geom_histogram(aes(x = L, fill = "L", alpha = .3),
+                     col = "white", bins = 70) 
+
+                 
