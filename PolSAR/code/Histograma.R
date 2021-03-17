3f2d0f6eda0efeefaaf242f0d3b0e80f2ac4c96c
leh11 <ui@laccan.ufal.br>
2021-03-17 19:40:15 -0300

    Curvas sobrepostos e ggpairs dos componentes

--- PolSAR/code/Histograma.R
+++ PolSAR/code/Histograma.R
@@ -11,7 +11,6 @@ require(distrEx)
 ## ==================================================================
 
 # read the ideal "pauli"
-
 ideal <- loadImage("../image/ideal/pauli.png", 
                    CIELab = TRUE, sample.size = NULL, ref.white = "D65",
                    lower = NULL, upper = NULL)
@@ -24,13 +23,16 @@ names(frame_RGB) <- c("R", "G", "B")
 
 all_points <- cbind(frame_LAB, frame_RGB)
 
+ggpairs(all_points, columns = 1:3,
+        upper = list(continuous = wrap("density", size=0.3)),
+        lower = list(continuous = wrap("points", size=0.0001, alpha=.3,
+                    col = rgb(all_points$R, all_points$G, all_points$B)), 
+                        combo = wrap("dot")
+        )
+) + theme_pander()
+
 # overlying the histogram using ggplot
 ggplot(all_points) +
-      geom_histogram(aes(x = a, y=..density.., fill = "a"), alpha = .3,
-                     col = "white", bins = 70) +
-      geom_histogram(aes(x = b, y=..density.., fill = "b"), alpha = .3,
-                     col = "white", bins = 70) +
-      geom_histogram(aes(x = L, y=..density.., fill = "L"), alpha = .3,
-                     col = "white", bins = 70) 
-
-                 
+  geom_density(aes(L, fill = "L"), alpha = .3) +
+  geom_density(aes(a, fill = "a"), alpha = .3) +
+  geom_density(aes(b, fill = "b"), alpha = .3)
